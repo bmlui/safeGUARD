b26cbf0c47242b2a3a2a6577edc32da1aa769636
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-04-08 01:47:49 +0000

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import firebase from '../firebase/clientApp';
+import GuestTableRow from '../components/GuestTableRow';
 
 export default function Home() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [color, setColor] = useState('');
   const [staffName, setStaffName] = useState('');
+
+
+  const [guests, setGuests] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchGuests = async () => {
+      const guestsCollection = await firebase.firestore().collection('guests').get();
+      const guestList = guestsCollection.docs.map((doc: any) => ({ id: doc.id, ...doc.data(), timestamp: doc.data().timestamp.toDate() }));
+      setGuests(guestList);
+    };
+
+    fetchGuests();
+  }, []);
+
+  const filteredGuests = guests.filter((guest) =>
+    guest.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    guest.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -43,11 +64,18 @@ export default function Home() {
     setFirstName('');
     setLastName('');
     setColor('');
-    alert("");
+    setStaffName("");
+    alert(guestKey + " has been added to the list.");
   };
+
+
+
+  
   
   return (
-    <div className="bg-gray-200 min-h-screen flex justify-center items-center">
+    <div>
+      <div className="bg-gray-200 h-24"></div>
+    <div className="bg-gray-200 flex justify-center items-center">
       <div className="max-w-lg w-full bg-white p-6 rounded-lg">
         <h1 className="text-2xl font-bold mb-4">Guest List</h1>
         <form onSubmit={handleSubmit}>
@@ -92,8 +120,7 @@ export default function Home() {
               <option value="yellow">Yellow</option>
               <option value="red">Red</option>
             </select>
-          </div>
-
+          </div> 
           <div className="flex flex-col mb-4">
             <label htmlFor="lastName" className="mb-1 font-semibold">
               Staff Name:
@@ -113,5 +140,38 @@ export default function Home() {
         </form>
       </div>
     </div>
+
+    <div className="bg-gray-200 h-10"></div>
+     <div className="bg-gray-200  flex justify-center items-center">
+     <div className="max-w-2xl w-full bg-white p-6 rounded-lg">
+       <input
+         type="text"
+         placeholder="Search by name..."
+         className="border border-gray-300 rounded px-3 py-2 mb-4"
+         value={searchTerm}
+         onChange={(event) => setSearchTerm(event.target.value)}
+       />
+       <table className="table-auto w-full">
+         <thead>
+           <tr>
+             <th className="px-4 py-2">First</th>
+             <th className="px-4 py-2">Last</th>
+             <th className="px-4 py-2">Color</th>
+             <th className="px-4 py-2">Date</th>
+             <th className="px-4 py-2">Staff</th>
+             
+           </tr>
+         </thead>
+         <tbody>
+           {filteredGuests.map((guest) => (
+             <GuestTableRow key={guest.id} guest={guest} />
+           ))}
+         </tbody>
+       </table>
+     </div>
+   </div>
+
+   <div className="bg-gray-200 h-24"></div>
+   </div>
   );
 }
