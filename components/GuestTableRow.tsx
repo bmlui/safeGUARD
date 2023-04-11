512fa5e67911824f9b6ab285ecea3a34f83efe40
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-04-11 22:01:30 +0000

    mobile view

--- components/GuestTableRow.tsx
+++ components/GuestTableRow.tsx
@@ -26,34 +26,65 @@ export default function GuestTableRow({ guest }: GuestTableRowProps) {
     }
   };
 
+  const nothing = () => {
+    //console.log("nothing");
+  }
   return (
+
     <tr className={deleted ? 'text-gray-400 line-through' : ''}>
-      <td className="uppercase">{guest.firstName}</td>
-      <td className="uppercase">{guest.lastName}</td>
+      <td className="hidden md:table-cell">{guest.firstName}</td>
+      <td className="hidden md:table-cell">{guest.lastName}</td>
       <td>
         <span
-          className={`px-2 py-1 rounded-lg font-bold text-white ${
+          className={`hidden md:table-cell px-2 py-1 rounded-lg font-bold text-white ${
             guest.color === 'green'
-              ? 'bg-green-500'
+              ? 'bg-emerald-100/60 text-emerald-500'
               : guest.color === 'yellow'
-              ? 'bg-yellow-500'
-              : 'bg-red-500'
+              ? 'bg-amber-100/60 text-amber-500'
+              : 'bg-red-100/60 text-red-500'
           }`}
         >
           {guest.color}
         </span>
       </td>
-      <td>{guest.timestamp.toDateString()}</td>
-      <td className="uppercase">{guest.staffName}</td>
-      <td>
+      <td className="hidden md:table-cell text-gray-500 whitespace-nowrap">{guest.timestamp.toLocaleDateString('en-US')}</td>
+      <td className="hidden md:table-cell uppercase whitespace-nowrap">{guest.staffName}</td>
+      <td className="hidden md:table-cell">
         <button 
           onClick={handleDelete}
           disabled={ deleted }
-          className={deleted ? 'bg-slate-200 text-slate-300  font-bold py-2 px-4 rounded' : 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'}
+          className={deleted ? 'text-slate-300 ' : ' text-red-500 hover:text-red-400 py-1'}
         >
           Delete
         </button>
-      </td>
+      </td >
+      <td className="md:hidden text-sm" id="mobiletable">
+      <div className="grid grid-cols-1 gap-4 block"> <a onClick={deleted ?  nothing : handleDelete}>
+    <div className="bg-white space-y-2 p-2 m-2 rounded shadow">
+    <div className="flex items-center space-x-2 text-sm">
+      <div className='font-bold text-base'>{guest.firstName} {guest.lastName}</div>
+      <div
+          className={`md:hidden px-2 py-1 rounded-lg font-semibold text-white ${
+            guest.color === 'green'
+              ? 'bg-emerald-100/60 text-emerald-500'
+              : guest.color === 'yellow'
+              ? 'bg-amber-100/60 text-amber-500'
+              : 'bg-red-100/60 text-red-500'
+          }`}
+        >
+          {guest.color}
+        </div>
+
+    </div>
+    <span className="text-gray-500">{guest.timestamp.toLocaleDateString('en-US')}</span>
+    <span className=''> {guest.staffName}</span>
+
+    </div>
+    </a>
+    </div>
+    </td>
+    
     </tr>
+   
   );
 }
